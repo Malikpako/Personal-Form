5e6655555b6230dbc3c8308b890ce2e3eeff3bbe
Malikpako <x0ulhacker786@gmail.com>
2017-11-12 13:38:08 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,90 @@
+
+<html>
+ 
+    <head>
+        
+        
+        
+        <meta charset="UTF-8">
+        <title> Form </title>
+        <link rel="stylesheet" type="text/css" href="Form.css">
+        
+        
+        
+    </head>
+    
+    
+    
+    <body>
+        
+        
+        <center>
+        <h1>Personal Form</h1>	
+
+        
+
+        <div id="Form"> </div>
+
+        <script type="text/javascript" src="Battleshio.js"></script>
+   
+    
+    
+        <form action="/action_page.php">
+  First name:
+  <input type="text" name="firstname" value="">
+  <br>
+  Last name:
+  <input type="text" name="lastname" value="">
+  <br>
+  Birth Date:
+   <select>
+  <option value="">Date</option>
+  
+  
+  
+  <option value="30">30</option>;
+  
+  }
+ 
+</select>
+  
+   <select>
+  <option value="Male">Month</option>
+  <option value="saab">Jan</option>
+  
+</select>
+  
+   <select>
+  <option value="Male">Year</option>
+  <option value="saab">Jan</option>
+  
+
+  <br><br>
+  
+
+
+        
+   Gender
+   <select>
+</select>
+       
+            Mobile No.: <input type="number" name="FirstName" value=""><br>
+            e.mail: <input type="text" name="LastName" value=""><br>
+
+
+        
+            Place Of Birth: <input type="text" name="PlaceOfBirth" value=""><br>
+        
+
+        <label for="male">Male</label>
+        <input type="radio" name="gender" id="male" value="male"><br>
+        <label for="female">Female</label>
+        <input type="radio" name="gender" id="female" value="female"><br>
+        <label for="other">Other</label>
+        <input type="radio" name="gender" id="other" value="other"><br><br>
+        <input type="submit" value="Submit">
+
+</form>
+ </center>
+    </body>
+</html>
